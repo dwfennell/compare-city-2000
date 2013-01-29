ec91454726f833902a81a13629e1592986b0fcbb
Dustin Fennell <dustin@fennd.org>
2013-01-28 19:38:16 -0800

    New models for city ranking

---
+++ App_Code/Model/CityRanking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompareCity.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CityRanking
+    {
+        public CityRanking()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
---
+++ App_Code/Model/CityRankingContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompareCity.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CityRankingContext
+    {
+        public CityRankingContext()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
