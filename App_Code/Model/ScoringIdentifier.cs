7f2d079ae516178a3a24f5b50085037487c8a2f6
Dustin Fennell <dustin@fennd.org>
2013-02-21 09:19:08 -0800

    Added new Entity Framework class to facilitate scoring identifiers in the db

---
+++ App_Code/Model/ScoringIdentifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// ScoringIdentifiers identify values that can be used in city scoring. 
+/// The values may be scalar or may be obtained through the use of a function with an input parameter.
+/// </summary>
+public class ScoringIdentifier
+{
+    public string Name { get; set; }
+
+    /// <summary>
+    /// A short description of the identifier, for display to user.
+    /// </summary>
+    public string Descrition { get; set; }
+
+    /// <summary>
+    /// The data type of the value refereced by this identifier. 
+    /// </summary>
+    public Type DataType { get; set; }
+
+    /// <summary>
+    /// Set to true if this scoring identifier is a function requiring an input parameter.
+    /// </summary>
+    public bool IsFunction { get; set; }
+
+    /// <summary>
+    /// The type expected as an input parameter.
+    /// Only applies if <see cref="IsFunction"/> is true.
+    /// </summary>
+    public Type InputParameterType { get; set; }
+}
